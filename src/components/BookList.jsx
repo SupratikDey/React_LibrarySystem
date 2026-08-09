fcf8c34d7cde1ec9ec2dfee3a49c15adfcd08609
SupratikDey <spratik.dey@gmail.com>
2026-08-09 16:16:33 +0530

    Filtering based on genre and search is now added

--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -1,26 +1,46 @@
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import { Items } from "../utils/MockData";
 import Book from "./Book";
 import "./Home.css";
 
-function BookList(){
+function BookList({Category}){
 
     const [Search , SetSearch] = useState("");
     const [Filter , Setfilter] = useState(Items);
+    const [cato,Setcato] = useState("");
 
-    function handleSearch(){
+    function Handlegenre(){
         const result = Items.filter((e)=>{
-            return (e.title.toLowerCase().includes(Search.toLowerCase()));            
+            return (e.genre.toLowerCase().includes(cato.toLowerCase()));            
         })
-        Setfilter(result);
+
+        const result1 = result.filter((e)=>{
+             return (e.title.toLowerCase().includes(Search.toLowerCase()));            
+         })
+
+        Setfilter(result1);
     }
+
+    // function handleSearch(){
+    //     const result = Items.filter((e)=>{
+    //         return (e.title.toLowerCase().includes(Search.toLowerCase()));            
+    //     })
+    //     Setfilter(result);
+    // }
+    useEffect(() => {
+        const result = Items.filter((e) => {
+            return e.genre.toLowerCase().includes(Category.toLowerCase());
+        });
+
+        Setfilter(result);
+    }, [Category]);
 
     return(
         <div>
             <div className="Search">
                 <h3>Search for books : </h3>
                 <input type="text" onChange={(e)=>SetSearch(e.target.value)}></input>
-                <button onClick={()=>{handleSearch()}}> Search </button>
+                <button onClick={()=>{Handlegenre()}}> Search </button>
             </div>
                 <div className="Book_display">
                     {Filter.map((e) => {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,17 @@
 import "./Home.css";
 import BookList from "./BookList";
+import { useState } from "react";
 
 function Home(){
+    const [category, setCategory] = useState("");
     return(
         <div>
             <div className="Heading">
                 <h2>WELCOME TO THE LIBRARY</h2>
                 <div className="End">
                     <h4>Categories :</h4>
-                    <select>
-                        <option value="Popular">Popular</option>
+                    <select onChange={(e) => setCategory(e.target.value)}>
+                        <option value="Fantasy">Fantasy</option>
                         <option value="Fiction">Fiction</option>
                         <option value="Non-Fiction">Non-Fiction</option>
                         <option value="Sci-Fi">Sci-Fi</option>
@@ -18,7 +20,7 @@ function Home(){
                 
             </div>
 
-            <BookList />
+            <BookList Category={category} />
         </div>
         
     )
